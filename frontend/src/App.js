3a60cd567c8c7c18a164bf9c5a63527f4a612e24
jkol7 <jkolcun7@gmail.com>
2022-08-01 14:25:53 -0400

    Added functions to determine winner and getting into if there is no instant win

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,11 @@ export default function App() {
     //Need to figure out how to deal with cpu dice. Use the same dice as other player? 
     
     
-    const [currentTurn, setCurrentTurn] = React.useState(Math.random() < 0.5 ? 'player' : 'cpu')
+    const [playerTurn, setPlayerTurn] = React.useState(Math.random() < 0.5 ? true : false)
     const [dice, setDice] = React.useState(allNewDice("player"))
     const [winRoll, setWinRoll] = React.useState(false)
     const [computerDice, setComputerDice] = React.useState(allNewDice("cpu"))
-    const [status, setStatus] = React.useState(`It is the ${currentTurn}'s turn`)
+    const [status, setStatus] = React.useState(playerTurn ? `It is the player's turn` : `It is the cpu's turn` )
     const [playerRoundPoint, setPlayerRoundPoint] = React.useState(0)
     const [computerRoundPoint, setComputerRoundPoint] = React.useState(0)
     const [playerScore, setPlayerScore] = React.useState(0)
@@ -23,171 +23,195 @@ export default function App() {
 
 
     React.useEffect(() => {
+        let diceToCheck
         const allDiceValues = dice.map(element => element.value)
         const allCPUDiceValues = computerDice.map(element => element.value)
        
+        checkFourFiveSix()
+        checkTriple()
+        checkPairWithSix()
+        checkOneTwoThree()
+        checkPairWithOne()
 
-        //Have to set new turn outside in case of draw
-        //Or have state where button needs to be pressed
+        if (winRoll !== true){
+            compareRoundPoints()
+            /*
+           if (playerRoundPoint > computerRoundPoint) {
+                console.log(`Player wins. Congrats you're here. Current player point ${playerRoundPoint} and cpu: ${computerRoundPoint}`)
+               setPlayerScore(prev => prev + 1)
+                setPlayerTurn(prev => !prev)
+            }
 
+           if (computerRoundPoint > playerRoundPoint) {
+            
+            console.log(`CPU wins. Congrats you're here. Current player point ${playerRoundPoint} and cpu: ${computerRoundPoint}`)
+           setCPUScore(prev => prev + 1)
+            setPlayerTurn(prev => !prev)
 
-        //Disable button if computer turn
+         }
+
+       if (computerRoundPoint === playerRoundPoint) {
+        console.log(`Draws. Congrats you're here. Current player point ${playerRoundPoint} and cpu: ${computerRoundPoint}`)
+        setPlayerTurn(prev => !prev)
+    
+    }
+*/
+    }
 
       /*  if (currentTurn === 'cpu'){
         document.querySelector(".roll-dice").setAttribute("disabled", true)
         }
 */
+
+
         // Check instant win (4,5,6)
 
-        if (currentTurn === 'player'){
-            if (allDiceValues.includes(4) && allDiceValues.includes(5) && allDiceValues.includes(6)){
+        function checkFourFiveSix(){
+            playerTurn ? diceToCheck = allDiceValues : diceToCheck = allCPUDiceValues
+            if (diceToCheck.includes(4) && diceToCheck.includes(5) && diceToCheck.includes(6)){
+                playerTurn? setStatus(`Player wins. Rolled 4, 5, 6. CPU turn.`) : setStatus(`CPU wins. Rolled 4, 5, 6. Player turn.`)
                 setWinRoll(true)
-                setPlayerScore(prev => prev + 1)
-                setCurrentTurn('cpu') 
-                setStatus(`Player wins. Rolled 4, 5, 6. CPU's turn.`)
-
+                playerTurn ? setPlayerScore(prev => prev + 1) : setCPUScore(prev => prev + 1)
+                setPlayerTurn(prev => !prev) 
             }
         }
 
-        if (currentTurn === 'cpu'){
-            if (allCPUDiceValues.includes(4) && allCPUDiceValues.includes(5) && allCPUDiceValues.includes(6) ){
+        // Check instant win (Triple)
+
+        function checkTriple(){
+            playerTurn ? diceToCheck = allDiceValues : diceToCheck = allCPUDiceValues
+            if (diceToCheck.every(value => value === diceToCheck[0])){
+                playerTurn? setStatus(`Player wins. Rolled triples. CPU turn.`) : setStatus(`CPU wins. Rolled triples. Player turn.`)
                 setWinRoll(true)
-                setCPUScore(prev => prev + 1)    
-                setCurrentTurn('player')  
-                setStatus(`CPU wins. Rolled 4, 5, 6. Player's turn.`)
-            }
-    }
-
-        // Check instant triple win
-
-        if (currentTurn === 'player'){
-            if (allDiceValues.every(value => value === allDiceValues[0])){
-                setWinRoll(true)
-                setPlayerScore(prev => prev + 1)
-                setCurrentTurn('cpu') 
-                setStatus(`Player wins. Rolled triples. CPU's turn.`)
-
+                playerTurn ? setPlayerScore(prev => prev + 1) : setCPUScore(prev => prev + 1)
+                setPlayerTurn(prev => !prev) 
             }
         }
 
+        // Check instant win (Pair with a six)
 
-        if (currentTurn === 'cpu'){
-            if (allCPUDiceValues.every(value => value === allCPUDiceValues[0])){
+        function checkPairWithSix(){
+            playerTurn ? diceToCheck = allDiceValues : diceToCheck = allCPUDiceValues
+            if(diceToCheck.includes(6)){
+                diceToCheck.sort()
+            }
+
+            if (diceToCheck[2] === 6 && diceToCheck[0] === diceToCheck[1]){
                 setWinRoll(true)
-                setCPUScore(prev => prev + 1)
-                setCurrentTurn('player') 
-                setStatus(`CPU wins. Rolled triples. Player's turn.`)
-
+                playerTurn? setStatus(`Player wins. Pair with 6. CPU's turn.`) : setStatus(`CPU wins. Pair with 6. Player's turn`)
+                playerTurn ? setPlayerScore(prev => prev + 1) : setCPUScore(prev => prev + 1)
+                setPlayerTurn(prev => !prev) 
             }
         }
-
-        //Check instant pair + 6 win
-
-        if (currentTurn === 'player' && allDiceValues.includes(6)){
-           
-            allDiceValues.sort()
-
-            if (allDiceValues[2] === 6 && allDiceValues[0] === allDiceValues[1]){
-                setWinRoll(true)
-                setPlayerScore(prev => prev + 1)
-                setCurrentTurn('cpu') 
-                setStatus(`Player wins. Pair + 6. CPU's turn.`)
-
-            }
-        }
-
-
-        if (currentTurn === 'cpu' && allCPUDiceValues.includes(6)){
-           
-            allCPUDiceValues.sort()
-       
-             if (allCPUDiceValues[2] === 6 && allCPUDiceValues[0] === allCPUDiceValues[1]){
-                 setWinRoll(true)
-                 setCPUScore(prev => prev + 1)
-                 setCurrentTurn('player') 
-                 setStatus(`CPU wins. Pair + 6. Player's turn.`)
-             }
-         }
-
+        
             // Check instant loss - 1, 2, 3
 
-         
-
-            if (currentTurn === 'player'){
-                if (allDiceValues.includes(1) && allDiceValues.includes(2) && allDiceValues.includes(3)){
+            function checkOneTwoThree(){
+                playerTurn ? diceToCheck = allDiceValues : diceToCheck = allCPUDiceValues
+                if (diceToCheck.includes(1) && diceToCheck.includes(2) && diceToCheck.includes(3)){
+                    playerTurn? setStatus(`Player loses. Rolled 1, 2, 3. CPU's turn.`) : setStatus(`CPU loses. Rolled 1, 2, 3. Player's turn.`)
                     setWinRoll(true)
-                    setCPUScore(prev => prev + 1)
-                    setCurrentTurn('cpu') 
-                    setStatus(`Player loses. Rolled 1, 2, 3. CPU's turn`)
-    
+                    playerTurn ? setCPUScore(prev => prev + 1) : setPlayerScore(prev => prev + 1)
+                    setPlayerTurn(prev => !prev) 
+
+            }
+        }
+
+            // Check instant loss - Pair + 1
+
+            function checkPairWithOne(){
+                playerTurn ? diceToCheck = allDiceValues : diceToCheck = allCPUDiceValues
+
+                if (diceToCheck.includes(1)){
+                diceToCheck.sort()
+                }
+
+                if (diceToCheck[0] === 1 && diceToCheck[1] === diceToCheck[2]){
+                    setWinRoll(true)
+                    playerTurn? setStatus(`Player loses. Rolled Pair with 1. CPU's turn.`) : setStatus(`CPU loses. Rolled Pair with 1. Player's turn`)
+                    playerTurn ? setCPUScore(prev => prev + 1) : setPlayerScore(prev => prev + 1)
+                    setPlayerTurn(prev => !prev) 
     
                 }
             }
-    
-            if (currentTurn === 'cpu'){
-                if (allCPUDiceValues.includes(1) && allCPUDiceValues.includes(2) && allCPUDiceValues.includes(3) ){
-                    setWinRoll(true)
-                    setPlayerScore(prev => prev + 1)    
-                    setCurrentTurn('player')  
-                    setStatus(`CPU loses. Rolled 1, 2, 3. Player's turn`)
-                
+
+
+            function compareRoundPoints(){
+
+                let playerPairCount = 0
+                let computerPairCount = 0
+                let playerPairDice = []
+                let computerPairDice = []
+
+                for (let i = 0; i < allDiceValues.length; i++){
+                    if (allDiceValues[i] === allDiceValues[i+1]){
+                        playerPairCount++
+                        playerPairDice.push(allDiceValues[i])
+                        playerPairDice.push(allDiceValues[i + 1])
+                    }
+                    if (allCPUDiceValues[1] === allCPUDiceValues[i+1]){
+                        computerPairCount++
+                        computerPairDice.push(allCPUDiceValues[i])
+                        computerPairDice.push(allCPUDiceValues[i + 1])
+                    }}
+
+                if (computerPairCount < 1 && playerPairCount < 1 ){
+                        console.log("Need to re-roll")
+                        return
+                    }
+
+                else if (computerPairCount === playerPairCount){
+                        console.log("Need to determine higher pair")
+                        return
                 }
-        }
 
+                else if (playerPairCount > computerPairCount){
+                    console.log("Player won the count")
+                    /*let filtered = diceToCheck.filter(value => value !== pairDice[0])
+                    playerTurn ? setPlayerRoundPoint(filtered) : setComputerRoundPoint(filtered)
+                   playerTurn ? setStatus(`Player rolled pair + ${playerRoundPoint}`) : setStatus(`CPU rolled pair + ${computerRoundPoint}`)
+                    setPlayerTurn(prev => !prev) */
+                }
+                else if (computerPairCount > playerPairCount){
+                    console.log("Computer won the count")
+                }
 
-                    // Check instant loss - Pair + 1
-
-
-                    if (currentTurn === 'player' && allDiceValues.includes(1)){
-           
-                        allDiceValues.sort()
             
-                        if (allDiceValues[0] === 1 && allDiceValues[1] === allDiceValues[2]){
-                            setWinRoll(true)
-                            setCPUScore(prev => prev + 1)
-                            setCurrentTurn('cpu') 
-                            setStatus(`Player loses. Pair + 1. CPU's turn.`)
-            
-                        }
-                    }
-            
-            
-                    if (currentTurn === 'cpu' && allCPUDiceValues.includes(1)){
-           
-                        allCPUDiceValues.sort()
-            
-                        if (allCPUDiceValues[0] === 1 && allCPUDiceValues[1] === allCPUDiceValues[2]){
-                            setWinRoll(true)
-                            setCPUScore(prev => prev + 1)
-                            setCurrentTurn('player') 
-                            setStatus(`CPU loses. Pair + 1. Player's turn.`)
-            
-                        }
-                    }
 
+        
+            }   
 
-                    // Check round point
+            /* Check round point if there is a double and not an instant win
 
-                    if (currentTurn === 'player'){
+                function roundPoint(){
+                    
                     let pairCount = 0
                     let pairDice = []
-                    for (let i = 0; i < allDiceValues.length; i++){
-                        if (allDiceValues[i] === allDiceValues[i+1]){
+
+                    playerTurn ? diceToCheck = allDiceValues : diceToCheck = allCPUDiceValues
+
+                    for (let i = 0; i < diceToCheck.length; i++){
+                        if (diceToCheck[i] === diceToCheck[i+1]){
                             pairCount++
-                            pairDice.push(allDiceValues[i])
-                            pairDice.push(allDiceValues[i + 1])
+                            pairDice.push(diceToCheck[i])
+                            pairDice.push(diceToCheck[i + 1])
                         }
                     
                     if (pairCount === 1){
-                        let filtered = allDiceValues.filter(value => value !== pairDice[0])
-                        setPlayerRoundPoint(filtered)
-                        setStatus(`Player rolled pair + ${playerRoundPoint}`)
+                        let filtered = diceToCheck.filter(value => value !== pairDice[0])
+                        playerTurn ? setPlayerRoundPoint(filtered) : setComputerRoundPoint(filtered)
+                      //  playerTurn ? setStatus(`Player rolled pair + ${playerRoundPoint}`) : setStatus(`CPU rolled pair + ${computerRoundPoint}`)
+                        setPlayerTurn(prev => !prev) 
                     }
+
+        
+                   
                     }
                 }   
+*/
         
 
-    }, [dice, currentTurn])
+    }, [dice])
 
 
 
@@ -215,6 +239,7 @@ export default function App() {
         
         setDice(allNewDice("player"))
         setComputerDice(allNewDice("cpu"))
+        setWinRoll(false)
 
       
         //  setDice(oldDice => oldDice.map(die => generateNewDie("player")))
