3bb0aa52267eb3265e86d8b66166d05c973d80c0
jkol7 <jkolcun7@gmail.com>
2022-08-04 15:26:12 -0400

    Second deploy

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use('/', require('./routes/gameRoutes'))
 
 if (proccess.env.NODE_ENV === 'production'){
-    app.use(express.static('../frontend/build'))
+    app.use(express.static('frontend/build'))
 }
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
