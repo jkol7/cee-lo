fa9e300c8562f18542c6c63a282d47e76fcb81d1
jkol7 <jkolcun7@gmail.com>
2022-08-04 16:11:08 -0400

    Routes

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.use(express.static('public'))
-
 if (proccess.env.NODE_ENV === 'production'){
     app.use(express.static('frontend/build'))
 }
